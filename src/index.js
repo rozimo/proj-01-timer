53062d631c3910d7546098af20553639ca172fb7
rozimo <rozieboee@gmail.com>
2026-04-15 18:06:49 -0500

    HTML and CSS completed

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 520,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
